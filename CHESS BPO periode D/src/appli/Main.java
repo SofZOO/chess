42edd91d355272d604aef010f2879d2effb63dc0
elyes <75447778+nuitinfo-prog@users.noreply.github.com>
2021-04-26 19:27:34 +0200

    je commit

--- CHESS BPO periode D/src/appli/Main.java
+++ CHESS BPO periode D/src/appli/Main.java
@@ -1,15 +1,19 @@
 package appli;
 
+
 import piece.FabriquePiece;
 import échiquier.Plateau;
-
+import sun.audio.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
     public static void jouer(){
 
     }
 //TODO : penser a faire l'affichage des pièces mangées par les joueurs
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Joueur jBlanc = new Joueur("BLANC",true,new FabriquePiece());
         Joueur jNoir = new Joueur("NOIR",false,new FabriquePiece());
@@ -33,5 +37,18 @@ public class Main {
         System.out.println(p);
         p.déplacer("e8e7",jNoir,jBlanc);
         System.out.println(p);
+
+
+        Thread.sleep(1000);
+        try
+        {
+            InputStream in = new FileInputStream("audio/tennisball.wav");
+            AudioStream as = new AudioStream(in);
+            AudioPlayer.player.start(as);
+        }
+        catch (IOException e)
+        {
+            System.err.println(e);
+        }
     }
 }
